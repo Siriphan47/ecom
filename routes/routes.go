57ca17fb38750f248e4f5e6c6271060d7e516d55
Siriphan47 <siriphansithisack@gmail.com>
2024-10-21 03:27:58 +0700

    assignment

---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/Maryu47/ecommerce-bill/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.Signup())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.POST("/users/productview", controllers.SearchProduct())
+	incomingRoutes.POST("/users/search", controllers.SearchProductByQuery())
+}
